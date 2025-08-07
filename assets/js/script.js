8a8277f731510bd24d1625978ffdff1d89dba3cc
Md-Minhazul-Alam <minhaz.alam2014@gmail.com>
2025-08-08 00:24:46 +0600

    mobile menu fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,11 @@ function toggleMobileMenu(){
     overlay.classList.add('active');
     document.body.style.overflow = 'hidden';
 }
+function closeMobileMenu(){
+    const sidebar = document.getElementById('mobileSidebar');
+    const overlay = document.querySelector('.mobile-overlay');
+    
+    sidebar.classList.remove('active');
+    overlay.classList.remove('active');
+    document.body.style.overflow = 'auto';
+}
